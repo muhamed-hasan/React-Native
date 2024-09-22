333d7682eef4667db224cc0a9580c2fd6a6a77fb
Mohamed Mostafa <83196077+muhamed-hasan@users.noreply.github.com>
2024-09-22 23:41:28 +0000

    n

---
+++ windows/rn/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::rn::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::rn::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
